1c006708f35f972c38e4ac4acde11adeec786ccc
pmk <mungyry1@naver.com>
2022-01-11 23:06:37 +0900

    first jsp

---
+++ HelloWorld/src/gui/HelloWorld.java
@@ -0,0 +1,51 @@
+package gui;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get은 url 주소 요청시 , 응답(respose, res)
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8"); // 한글설정
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<b>Hello World!!!</b><br>");
+		out.println("<b>안녕하세요!!!</b>");
+		out.println("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ HelloWorld/src/gui/Hijsp.java
@@ -0,0 +1,36 @@
+package gui;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// HttpServlet �� ����� Ŭ���� => ����
+@WebServlet("/hi")
+public class Hijsp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// get은 url 주소 요청시 , 응답(respose, res)
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<b>hi</b>");
+		out.println("</html>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// form 요청시 예)로그인 (주소창에 표시되지 않음)
+		super.doPost(req, resp);
+	}
+	
+	
+
+}
